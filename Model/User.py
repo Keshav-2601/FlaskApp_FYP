8d1286fc2b9e5e1078a41cfcaa70689604f411db
Keshav <keshavv857@gmail.com>
2026-02-01 18:01:00 +0000

    completed login and create functionality

--- Model/User.py
+++ Model/User.py
@@ -1,24 +1,25 @@
 from flask import Flask
-from Pages import app 
-from app import db ## coming from app or main page.
-from flask_bcrypt import Bcrypt
+from Pages.extension import bcrypt,db
 class User(db.Model): # inherit from db.model db is the obj got from sqlAlchemy in flask
-    __tablename__ = "users"
+    __tablename__ = "users" 
     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-    name = db.Column(db.String(100), nullable=True)
+    name=db.Column(db.String(120),nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     address=db.Column(db.String(120),unique=False,nullable=True)
     password_hash = db.Column(db.String(255), nullable=False)
 
-    def __init__(self,email,address,password):  ## parametrized constructor here. self is like the this 
+    def __init__(self,name,email,address,password): ## parametrized constructor here. self is like the this  
+        self.name=name
         self.email=email
         self.address=address
-        self.password=password
+        self.password_hash=password
 
-
-    def hashpassowrd(password):
-        bcrypt = Bcrypt(app)
-        password_hash=bcrypt.generate_password_hash(password)
-        
-        return password_hash
+    @staticmethod
+    def hashpassword(password):
+         password_hash=bcrypt.generate_password_hash(password).decode("utf-8")
+         return password_hash
+    
+    @staticmethod
+    def checkhashpassword(password_hash,password):
+       return bcrypt.check_password_hash(password_hash,password)
     
--- Pages/app.py
+++ Pages/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask import Request,Response,jsonify
+from flask import request,jsonify
+
 from dotenv import load_dotenv
+from email_validator import validate_email, EmailNotValidError
+from Pages.extension import db,bcrypt,migrate,jwt_manager
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
+from Model.User import User
+
 import os
 
 
@@ -14,16 +20,67 @@ CORS(app, origins=["http://localhost:3000"])
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 
-db = SQLAlchemy(app)
 
-@app.get("/createuser")
+
+db.init_app(app)
+bcrypt.init_app(app)
+migrate.init_app(app,db)
+jwt_manager.init_app(app)
+@app.post("/createuser")
 def createuser():
-    body=jsonify(Request)
-    print(body)
-    
-    return "Flask is running"
+    body=request.get_json() 
 
-@app.get("/login")
-def login(req,res):
-    return "login is correct"
+    email=body["email"]
+    address=body["address"]
+    password=body["password"]
+    name=body["name"]
+    if not email or not password:
+       return jsonify({"response":"email or password is missing "}),400
+    
+    if not validEmail(email):
+       return jsonify({"response":"not a valid email"}),400
+    
+    hash_password=User.hashpassword(password)
+
+    newuser=User(name,email,address,hash_password)
+
+    db.session.add(newuser)
+    db.session.commit()
+
+    return jsonify({
+       "response":"created a new user succefully"
+    }),201
+
+@app.post("/login")
+def login():
+   body=request.get_json()
+   myemail=body["email"]
+   mypassword=body["password"]
+
+   Myuser=User.query.filter_by(email=myemail).first()
+   
+   if not Myuser:
+      return jsonify({"response":"wrong email"}),400
+   get_hash_password=Myuser.password_hash
+
+   if not User.checkhashpassword(get_hash_password,mypassword):
+      return jsonify({"response":"password is incorect"}),400
+   
+   myjwttoken=create_access_token(
+      identity={"userId":Myuser.id,"email":Myuser.email},
+      expires_delta=timedelta(minutes=1)
+      
+      )
+
+   return jsonify({"response":"correct credential hence login successfully!!","token":myjwttoken}),200
+   
+
+
+def validEmail(email):
+   try:
+      validate_email(email)
+      return True
+   except EmailNotValidError:
+      return False
